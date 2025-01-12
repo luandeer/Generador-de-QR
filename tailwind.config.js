d6199b6141e225082cf054cb3820458313f799a8
alexander <alexander_pere@hotmail.com>
2025-01-11 20:54:38 -0500

    generador de QR

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html", "./js/**/*.js", "./qrcodejs-master/**/*.js", "./css/**/*.css"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
